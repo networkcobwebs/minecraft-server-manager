65e2638d55d4652b80e994840fad4bc064b47fb2
Nick Rogers <rnrogers@cobaltiron.com>
2017-12-21 22:00:33 -0600

    WIP

--- MinecraftServerManager/app/model/Preferences.js
+++
@@ -1,9 +0,0 @@
-
-Ext.define('MinecraftServerManager.model.Preferences', {
-    extend: 'Ext.data.Model',
-
-    fields: [
-        { name: 'minecraftPath',    type: 'string' },
-        { name: 'isNode',           type: 'boolean' }
-    ]
-});
--- MinecraftServerManager/app/view/minecraftserver/ServerPropertiesModel.js
+++
@@ -1,9 +0,0 @@
-
-Ext.define('MinecraftServerManager.view.minecraftserver.ServerPropertiesModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.minecraftserverproperties',
-
-    data: {
-        /* This object holds the arbitrary data that populates the ViewModel and is then available for binding. */
-    }
-});
