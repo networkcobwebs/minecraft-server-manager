70f02d1ad3c97718f3bb0c63c18293fe3468bd22
Nick Rogers <nickrogers@NickRs-MacBook-Pro.local>
2019-01-14 12:48:08 -0600

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2015
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-console": "off",
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
