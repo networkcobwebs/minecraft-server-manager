d790ea0299a1adf1d05ce5b08b1ed8ef83d74a38
Nick Rogers <nrogers@mobileupsoftware.com>
2019-06-29 15:53:58 -0500

    property ui updates;

--- minecraftservermanager/src/ServerControls/ServerProperties.js
+++ minecraftservermanager/src/ServerControls/ServerProperties.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+import axios from 'axios';
 
 import 'typeface-roboto';
 import Button from '@material-ui/core/Button';
@@ -15,10 +17,85 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Typography from '@material-ui/core/Typography';
 
+import ActionInProgressDialog from './ActionInProgressDialog.js';
+
 export default function ServerProperties (props) {
-    const { minecraftProperties } = props;
+    const currentMinecraftProperties = Object.assign({}, props.minecraftProperties);
+    const [dirtyProps, setDirtyProps] = useState(false);
+    const [minecraftProperties, setMinecraftProperties] = useState(props.minecraftProperties);
+    const [progressDialogOpen, setProgressDialogOpen] = useState(false);
+    
+    const openProgressDialog = () => {
+        setProgressDialogOpen(true);
+    };
+    
+    const closeProgressDialog = () => {
+        setProgressDialogOpen(false);
+    };
+
+    const refreshProperties = () => {
+        openProgressDialog();
+        axios({
+            method: 'get',
+            url: `/api/refreshServerProperties`
+        }).then(() => {
+            setMinecraftProperties(props.minecraftProperties);
+            setProgressDialogOpen(false);
+        }, err => {
+            console.log('An error occurred contacting the Minecraft server.', err);
+            setProgressDialogOpen(false);
+        });
+    };
+
+    const saveProperties = () => {
+        openProgressDialog();
+        axios({
+            method: 'post',
+            url: `/api/saveMinecraftProperties`,
+            params: {
+                properties: minecraftProperties
+            }
+        }).then(() => {
+            refreshProperties();
+            setDirtyProps(false);
+        }, err => {
+            console.log('An error occurred contacting the Minecraft server.', err);
+            refreshProperties();
+        });
+    };
+
+    const undoPropertyEdits = () => {
+        setMinecraftProperties(currentMinecraftProperties);
+        setDirtyProps(false);
+    };
+
+    const updatePropertyType = (event) => {
+        let newMinecraftProps = Object.assign({}, minecraftProperties);
+        let newServerProps = newMinecraftProps.serverProperties;
+        let changed = false;
+        let property;
+        for (let p = 0; p < newServerProps.length; p++) {
+            property = newServerProps[p];
+            if (property.name === event.target.id) {
+                if (property.value !== event.target.value) {
+                    property.value = event.target.value;
+                    changed = true;
+                }
+                break;
+            }
+        }
+        if (changed){
+            setMinecraftProperties(newMinecraftProps);
+            setDirtyProps(true);
+        }
+    };
+
     return (
         <div>
+            <ActionInProgressDialog
+                open = { progressDialogOpen }
+                onClose = { closeProgressDialog }
+            />
             <Typography variant="subtitle1">
                 Server Properties
             </Typography>
@@ -34,15 +111,26 @@ export default function ServerProperties (props) {
             <Typography>
                 <strong>Refresh</strong> reads current values from the Minecraft server files.
             </Typography>
-            <Button disabled variant="contained" color="primary">
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={ refreshProperties }>
                 <Refresh />
                 Refresh
             </Button>
-            <Button disabled variant="contained" color="primary">
+            <Button
+                disabled = { !dirtyProps }
+                variant="contained"
+                color="primary"
+                onClick = { saveProperties }>
                 <Save />
                 Save
             </Button>
-            <Button disabled variant="contained" color="primary">
+            <Button
+                disabled = { !dirtyProps }
+                variant="contained"
+                color="primary"
+                onClick = { undoPropertyEdits }>
                 <Undo />
                 Undo
             </Button>
@@ -62,7 +150,9 @@ export default function ServerProperties (props) {
                                             <Input
                                                 id = { property.name }
                                                 value = { property.value }
-                                                fullWidth />
+                                                fullWidth
+                                                onChange = { updatePropertyType }
+                                                onKeyPress = { updatePropertyType }/>
                                         </FormControl>
                                     </TableCell>
                                 </TableRow>
