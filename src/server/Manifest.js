981d0f0a1b2da35a5c67706029c1652f6a32ed94
Dev_Bon_Bon <frapul2gar@live.fi>
2019-09-07 20:29:38 +0300

    Manifest: Add `fetch()`
    Also adds the static property `Manifest.types`

--- src/server/Manifest.js
+++ src/server/Manifest.js
@@ -1,3 +1,4 @@
+const https = require('https');
 /**
  * Map, that can fetch, parse, download and update Minecraft Server versions
  * @extends Map
@@ -16,8 +17,32 @@ class Manifest extends Map {
   get url () {
     return this.customUrl || Manifest.url;
   }
+
+  /**
+   * Fetches the Minecraft version manifest from "this.url" and parses it
+   * @return {Promise} Resolves to 'this' or rejects with an Error object
+   */
+  fetch () {
+    return new Promise((resolve, reject) => {
+      https.get(this.url, (response) => {
+        let data = '';
+        response.on('data', chunk => { data += chunk; });
+        response.on('end', () => {
+          JSON.parse(data).versions.forEach(({ id, type, url, releaseTime }) => {
+            if (Manifest.types.has(type)) {
+              this.set(id, { type, url, age: Date.parse(releaseTime) });
+            }
+          });
+          resolve(this);
+        });
+      }).on('error', reject);
+    });
+  }
 }
 // Default manifest URL string, so as not to store copies in every instance
 Manifest.url = new URL('https://launchermeta.mojang.com/mc/game/version_manifest.json');
+// The default Set of version type strings to include when parsing the manifest
+// NOTE: Should be per instance customizable, but not sure how to implement that
+Manifest.types = new Set(['release', 'snapshot']);
 
 module.exports = Manifest;
